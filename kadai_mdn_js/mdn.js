aaedb188f4ae9adbe286c77d8ed4e39d214210dc
satsuki-0122 <147584173+satsuki-0122@users.noreply.github.com>
2023-11-29 21:39:28 +0900

    Delete kadai_mdn_js/mdn.js

--- kadai_mdn_js/mdn.js
+++
@@ -1,8 +0,0 @@
-const date = new Date(2023, 8, 22);
-const [year, month, day] = [
-  date.getFullYear(),
-  date.getMonth(),
-  date.getDate()
-];
-
-console.log(year + '年' + month + '月' +  day + '日');
